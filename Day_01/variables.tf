d0a55ad9d935d7aa2780e0b77f507157b93847dc
Karan <166314486+KaranTomar16@users.noreply.github.com>
2026-01-06 14:34:39 +0530

    updated tfvars

--- Day_01/variables.tf
+++ Day_01/variables.tf
@@ -1,11 +1,10 @@
 variable "ami_id" {
     type = string 
-    default = "ami-068c0051b15cdb816" 
-    description = "passing value"
+    default = ""
+    
 }
 
 variable "instance_type" {
     type = string
-    default = "t2.micro"
-    description = "passing instance type"
+    default = ""
 }
