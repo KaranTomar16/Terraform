52addcaad41527a49623c39e80ea6ab9ec301170
Karan <166314486+KaranTomar16@users.noreply.github.com>
2026-01-22 17:18:23 +0530

    updated

--- Day_03/main.tf
+++ Day_03/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "second_instance" {
 
 } 
 
-resource "aws_s3_bucket" "test" {
+resource "aws_s3_bucket" "dev" {
   bucket = "sugsivskfhksfbskuvskdcfsgs"
   tags = {
     Name = "dev"
