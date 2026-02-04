9f4f7caee6f9ff28b21b126c4448c78deffb36ca
Karan <166314486+KaranTomar16@users.noreply.github.com>
2026-02-04 22:52:32 +0530

    test

--- Day_03/main.tf
+++ Day_03/main.tf
@@ -19,3 +19,15 @@ output "second_instance_public_ip" {
 output "test_bucket_name" {
   value = aws_s3_bucket.test.bucket
 }
+output "test_bucket_arn" {
+  value = aws_s3_bucket.test.arn
+}
+output "test_bucket_region" {
+  value = aws_s3_bucket.test.region
+}
+output "test_bucket_size" {
+  value = aws_s3_bucket.test.size
+}
+output "test_bucket_tags" {
+  value = aws_s3_bucket.test.tags
+}
